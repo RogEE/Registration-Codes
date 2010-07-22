59b66766547c006d5ec220990a2fa794137e275d
Michael Rog <michaelrog@users.noreply.github.com>
2010-07-21 22:02:02 -0700

    initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['name'] = 'RogEE Registration Codes';
+$config['version'] = '0.0.1';
+$config['nsm_addon_updater']['versions_xml'] = 'http://bitbucket.org/rogee/registration_codes/raw/tip/versions.xml';
+    
+/* End of file config.php */
+/* Location: ./system/expressionengine/third_party/registration_titles/config.php */
---
+++ language/english/lang.registration_codes.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+
+//----------------------------------------
+// BASIC INFORMATION ABOUT THE EXT
+//----------------------------------------
+
+"registration_codes_module_name" =>
+"RogEE Registration Codes",
+
+"registration_codes_module_description" =>
+"Automatically places new members into pre-specified groups according to registration codes.",
+
+//----------------------------------------
+
+'rogee_rc_language' => 'RogEE Registration Codes speaks English!',
+
+'rogee_rc_group_id' => 'Destination member group ID',
+'rogee_rc_code' => 'Registration code',
+
+// END
+''=>''
+);
+/* End of file lang.registration_codes.php */
+/* Location: ./system/expressionengine/third_party/registration_codes/language/english/lang.registration_codes.php */
