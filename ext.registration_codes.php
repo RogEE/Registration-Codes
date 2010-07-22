176d0fb554e6dad0d2acfed40df93b1849e72490
Michael Rog <michael@michaelrog.com>
2010-07-21 22:02:02 -0700

    initial commit

---
+++ ext.registration_codes.php
@@ -0,0 +1,39 @@
+<?php  
+
+/*
+=====================================================
+
+RogEE "Registration Codes"
+an extension for ExpressionEngine 2
+by Michael Rog
+v0.1
+
+email Michael with questions, feedback, suggestions, bugs, etc.
+>> michael@michaelrog.com
+
+This extension is compatible with NSM Addon Updater:
+>> http://github.com/newism/nsm.addon_updater.ee_addon
+
+Changelog:
+0.1 - dev
+
+=====================================================
+
+*/
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// -----------------------------------------
+//	Begin class
+// -----------------------------------------
+
+class Registration_codes_ext
+{
+
+
+
+		
+} // END CLASS
+
+/* End of file ext.registration_codes.php */
+/* Location: ./system/expressionengine/third_party/registration_codes/ext.registration_codes.php */
