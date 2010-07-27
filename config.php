62877f9792f7510fa2a1952b15b81caacce05bbe
Michael Rog <michaelrog@users.noreply.github.com>
2010-07-26 20:26:45 -0700

    feature-complete. still testing.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['name'] = 'RogEE Registration Codes';
-$config['version'] = '0.0.1';
+$config['version'] = '0.1.0';
 $config['nsm_addon_updater']['versions_xml'] = 'http://bitbucket.org/rogee/registration_codes/raw/tip/versions.xml';
     
 /* End of file config.php */
