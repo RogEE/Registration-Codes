5592dfd42e7362308586d2e97875076288e2afd1
Michael Rog <michaelrog@users.noreply.github.com>
2011-03-31 23:03:47 -0700

    moving versions xml out of repo, version increment

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 $config['name'] = 'RogEE Registration Codes';
-$config['version'] = '1.1.0';
-$config['nsm_addon_updater']['versions_xml'] = 'http://bitbucket.org/rogee/registration_codes/raw/tip/versions.xml';
+$config['version'] = '1.2.0';
+$config['nsm_addon_updater']['versions_xml'] = 'http://michaelrog.com/ee/versions/registration-codes';
     
 /* End of file config.php */
 /* Location: ./system/expressionengine/third_party/registration_titles/config.php */
