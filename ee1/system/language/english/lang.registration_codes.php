4500e69d9f8c0709dd36153eb47a2898aff3ae3a
Michael Rog <michael@michaelrog.com>
2011-07-07 12:47:49 -0500

    making progress on the settings form

---
+++ ee1/system/language/english/lang.registration_codes.php
@@ -0,0 +1,12 @@
+<?php
+  
+$L = array(
+
+'rogee_rc_name' => 'RogEE Registration Codes',
+
+'' => ''
+);
+// END array
+
+/* End of file lang.registration_codes.php */
+/* Location: ./system/language/english/lang.registration_codes.php */
